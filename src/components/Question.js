630f1f66488f1959bb507df3cc976f1e85c85fc4
Derrick Nyasio <dnyasio2014@gmail.com>
2022-10-05 12:49:12 +0300

    Assignment Done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
-  const [timeRemaining, setTimeRemaining] = useState(10);
+    const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+    // add useEffect code
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeRemaining(timeRemaining - 1)
+            if (timeRemaining === 0) {
+                setTimeRemaining(10);
+                onAnswered(false)
+            }
+        }, 1000);
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+        return function cleanup() {
+            clearTimeout(timer)
+        }
+    }, [timeRemaining, onAnswered]);
 
-  const { id, prompt, answers, correctIndex } = question;
+    function handleAnswer(isCorrect) {
+        setTimeRemaining(10);
+        onAnswered(isCorrect);
+    }
 
-  return (
-    <>
-      <h1>Question {id}</h1>
-      <h3>{prompt}</h3>
-      {answers.map((answer, index) => {
-        const isCorrect = index === correctIndex;
-        return (
-          <button key={answer} onClick={() => handleAnswer(isCorrect)}>
-            {answer}
-          </button>
-        );
-      })}
-      <h5>{timeRemaining} seconds remaining</h5>
-    </>
-  );
+    const { id, prompt, answers, correctIndex } = question;
+
+    return ( <
+            >
+            <
+            h1 > Question { id } < /h1> <
+            h3 > { prompt } < /h3> {
+            answers.map((answer, index) => {
+                const isCorrect = index === correctIndex;
+                return ( <
+                    button key = { answer }
+                    onClick = {
+                        () => handleAnswer(isCorrect)
+                    } > { answer } <
+                    /button>
+                );
+            })
+        } <
+        h5 > { timeRemaining }
+    seconds remaining < /h5> < / >
+);
 }
 
 export default Question;
